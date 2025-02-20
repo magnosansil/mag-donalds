b47cac99ca49c16d871b581298570f96cb35ef21
magnosansil <magnosansil@outlook.com>
2025-02-20 17:19:00 -0300

    Feat: Add Restaurant Menu Page With Categories And Products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { getRestaurantBySlug } from "@/data/getRestaurantBySlug";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -27,6 +28,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
--- src/data/getRestaurantBySlug.ts
+++ src/data/getRestaurantBySlug.ts
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 import { db } from "@/lib/prisma";
 
 export const getRestaurantBySlug = async (slug: string) => {
-  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  const restaurant = await db.restaurant.findUnique({ where: { slug }, include: { menuCategories: { include: { products: true } } } });
 
   if (!restaurant) {
     return notFound();
