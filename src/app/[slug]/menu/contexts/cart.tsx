501c8ce03e4a5417f8a7657a2f89283cc3e579ec
magnosansil <magnosansil@outlook.com>
2025-02-27 01:37:23 -0300

    Feat: Add Initial "Add To Cart" Function To Cart Context

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -3,7 +3,8 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Product {
+interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
   quantity: number;
 }
 
@@ -11,12 +12,14 @@ export interface ICartContent {
   isOpen: boolean;
   products: CartProduct[];
   toggleCart: () => void;
+  addProduct: (product: CartProduct) => void;
 }
 
 export const CartContext = createContext<ICartContent>({
   isOpen: false,
   products: [],
   toggleCart: () => {},
+  addProduct: () => {},
 });
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -24,8 +27,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleCart = () => setIsOpen((prev) => !prev);
+
+  const addProduct = (product: CartProduct) => {
+    setProducts((prev) => [...prev, product]);
+  };
   return (
-    <CartContext.Provider value={{ isOpen, products, toggleCart }}>
+    <CartContext.Provider value={{ isOpen, products, toggleCart, addProduct }}>
       {children}
     </CartContext.Provider>
   );
